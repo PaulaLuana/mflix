b31e7e85fe3cf047b7f204a5a0f4a39fd2852adf
Paula Luana <luana@alu.ufc.br>
2022-02-08 10:19:35 -0300

    busca de filme por país e elenco

--- src/main/java/mflix/api/daos/MovieDao.java
+++ src/main/java/mflix/api/daos/MovieDao.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -118,10 +119,14 @@ public class MovieDao extends AbstractMFlixDao {
      */
     public List<Document> getMoviesByCountry(String... country) {
 
-        Bson queryFilter = new Document();
-        Bson projection = new Document();
-        //TODO> Ticket: Projection - implement the query and projection required by the unit test
+        Bson queryFilter = new Document("countries", new Document("$all", Arrays.asList(country)));
         List<Document> movies = new ArrayList<>();
+        moviesCollection
+                .find(queryFilter)
+                .projection(new Document("title", 1).append("_id", 1))
+                .into(movies);
+        //TODO> Ticket: Projection - implement the query and projection required by the unit test
+
 
         return movies;
     }
@@ -162,16 +167,18 @@ public class MovieDao extends AbstractMFlixDao {
      * @return List of documents sorted by sortKey that match the cast selector.
      */
     public List<Document> getMoviesByCast(String sortKey, int limit, int skip, String... cast) {
-        Bson castFilter = null;
-        Bson sort = null;
-        //TODO> Ticket: Subfield Text Search - implement the expected cast
-        // filter and sort
+        // query filter
+        Bson castFilter = Filters.in("cast", cast);
+        // sort key
+        Bson sort = Sorts.descending(sortKey);
         List<Document> movies = new ArrayList<>();
+        // TODO > Ticket: Paging - implement the necessary cursor methods to support simple
+        // pagination like skip and limit in the code below
         moviesCollection
                 .find(castFilter)
                 .sort(sort)
-                .limit(limit)
                 .skip(skip)
+                .limit(limit)
                 .iterator()
                 .forEachRemaining(movies::add);
         return movies;
